5d91bf5a4291f81ee03d0bd38a33247017cdebdb
Teresa Lenz <teresa.lenz@desy.de>
2015-08-28 10:00:47 +0200

    mass and ctau array is included in variablesSettings.sh

--- variablesSetting.sh
+++ variablesSetting.sh
@@ -4,19 +4,23 @@ metCut=100
 jetptCut=110
 ecaloCut=5
 ptCutMin=30
-ptCutMax=500000000000000000000
+ptCutMax=50000
 iasCutMin=0.05
 iasCutMax=0.99
 region="SR"
 
+
+mass=("100" "200" "300" "400" "500")
+ctau=("1" "5" "10" "50" "100" "1000")                           #please specifiy in cm
+
 echo "config file is read"
 
-echo "jetPt cut = " $jetptCut
-echo "MET cut   = " $metCut
-echo "Ecalo cut = " $ecaloCut
-echo "Pt cut    = " $ptCutMin - $ptCutMax
-echo "Ias cut   = " $iasCutMin - $iasCutMax
-echo "region    = " $region
+#echo "jetPt cut = " $jetptCut
+#echo "MET cut   = " $metCut
+#echo "Ecalo cut = " $ecaloCut
+#echo "Pt cut    = " $ptCutMin - $ptCutMax
+#echo "Ias cut   = " $iasCutMin - $iasCutMax
+#echo "region    = " $region
 
 
 
