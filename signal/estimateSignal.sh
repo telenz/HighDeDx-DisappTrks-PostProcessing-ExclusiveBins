6918b47086eaa192b587942560ca37409e6889f0
Teresa Lenz <teresa.lenz@desy.de>
2015-08-28 09:58:19 +0200

    C script is conducted for every sample seperately

--- signal/estimateSignal.sh
+++ signal/estimateSignal.sh
@@ -2,9 +2,14 @@
 
 #source "../variablesSetting.sh"
 
-root -l -b -q getSignalResult.C+"(${metCut},${jetptCut},${ptCutMin},${ptCutMax},${ecaloCut},${iasCutMin},${iasCutMax},\"${region}\")" > signal.log
-#cat signal.log
-
+for ct in "${ctau[@]}"
+  do
+    for m in "${mass[@]}"
+    do
+	root -l -b -q getSignalResult.C+"(${metCut},${jetptCut},${ptCutMin},${ptCutMax},${ecaloCut},${iasCutMin},${iasCutMax},\"${region}\",\"Madgraph_signal_mass_${m}_ctau_${ct}cm\")" > logFiles/signal_mass_${m}GeV_ctau_${ct}cm.log
+	python makeSignalDatacard.py ${metCut} ${ptCutMin} ${ptCutMax} ${ecaloCut} "${iasCutMin}" "${iasCutMax}" ${m} ${ct}
+    done
+done
 rm *~
 #rm *_C.d
 #rm *_C.so
