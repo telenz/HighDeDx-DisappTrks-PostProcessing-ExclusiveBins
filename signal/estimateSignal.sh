86629b964a1ad4b23154d565c02c12a14e88f01f
Teresa Lenz <teresa.lenz@desy.de>
2015-09-07 11:04:45 +0200

    skip 1cm sample with m>200GeV

--- signal/estimateSignal.sh
+++ signal/estimateSignal.sh
@@ -6,6 +6,10 @@ for ct in "${ctau[@]}"
   do
     for m in "${mass[@]}"
     do
+	if [ "$ct" == "1" ] && ([ "$m" == "300" ] || [ "$m" == "400" ] || [ "$m" == "500" ])
+	then
+	    continue
+	fi
 	root -l -b -q getSignalResult.C+"(${metCut},${jetptCut},${ptCutMin},${ptCutMax},${ecaloCut},${iasCutMin},${iasCutMax},\"${region}\",\"Madgraph_signal_mass_${m}_ctau_${ct}cm\")" > logFiles/signal_mass_${m}GeV_ctau_${ct}cm.log
 	python makeSignalDatacard.py ${metCut} ${ptCutMin} ${ptCutMax} ${ecaloCut} "${iasCutMin}" "${iasCutMax}" ${m} ${ct}
     done
