5a1b20ec07f9e5b61df71b87ee8468f707aafd90
Teresa Lenz <teresa.lenz@desy.de>
2015-09-07 11:52:38 +0200

    skip 1cm sample with m>200GeV

--- systematics/combineDatacards.sh
+++ systematics/combineDatacards.sh
@@ -10,7 +10,10 @@ for m in "${mass[@]}"
 do
     for ct in "${ctau[@]}"
     do
-
+	if [ "$ct" == "1" ] && ([ "$m" == "300" ] || [ "$m" == "400" ] || [ "$m" == "500" ])
+	then
+	    continue
+	fi
       combineCards.py signal/datacards/datacard_metCutEq100_ptGt30_Le50_ECaloCutEq5_IasGt0p05_Le0p30_mass_${m}GeV_ctau_${ct}cm.txt signal/datacards/datacard_metCutEq100_ptGt30_Le50_ECaloCutEq5_IasGt0p30_Le0p99_mass_${m}GeV_ctau_${ct}cm.txt signal/datacards/datacard_metCutEq100_ptGt50_Le50000_ECaloCutEq5_IasGt0p05_Le0p30_mass_${m}GeV_ctau_${ct}cm.txt signal/datacards/datacard_metCutEq100_ptGt50_Le50000_ECaloCutEq5_IasGt0p30_Le0p99_mass_${m}GeV_ctau_${ct}cm.txt   > datacardsCombined/datacard_mass_${m}GeV_ctau_${ct}cm.txt
 
     done
