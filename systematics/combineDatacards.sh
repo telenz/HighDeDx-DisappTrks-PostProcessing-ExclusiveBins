d15e981b134b0471245d6215375f5e95e9e0fd0d
Teresa Lenz <teresa.lenz@desy.de>
2015-09-30 17:32:23 +0200

    600GeV changes

--- systematics/combineDatacards.sh
+++ systematics/combineDatacards.sh
@@ -4,23 +4,25 @@ cd ../limits/CMSSW_5_3_20/src/HiggsAnalysis-CombinedLimit/
 cmsenv
 cd -
 
+folder="signal/datacards"
+
 source ../variablesSetting.sh
 
 for m in "${mass[@]}"
 do
     for ct in "${ctau[@]}"
     do
-	if [ "$ct" == "1" ] && ([ "$m" == "300" ] || [ "$m" == "400" ] || [ "$m" == "500" ])
+	if [ "$ct" == "1" ] && ([ "$m" == "300" ] || [ "$m" == "400" ] || [ "$m" == "500" ] || [ "$m" == "600" ])
 	then
 	    continue
 	fi
-      combineCards.py signal/datacards/datacard_metCutEq100_ptGt30_Le50_ECaloCutEq5_IasGt0p05_Le0p30_mass_${m}GeV_ctau_${ct}cm.txt signal/datacards/datacard_metCutEq100_ptGt30_Le50_ECaloCutEq5_IasGt0p30_Le0p99_mass_${m}GeV_ctau_${ct}cm.txt signal/datacards/datacard_metCutEq100_ptGt50_Le50000_ECaloCutEq5_IasGt0p05_Le0p30_mass_${m}GeV_ctau_${ct}cm.txt signal/datacards/datacard_metCutEq100_ptGt50_Le50000_ECaloCutEq5_IasGt0p30_Le0p99_mass_${m}GeV_ctau_${ct}cm.txt   > datacardsCombined/datacard_mass_${m}GeV_ctau_${ct}cm.txt
+      combineCards.py ${folder}/datacard_metCutEq100_ptGt30_Le50_ECaloCutEq5_IasGt0p05_Le0p30_mass_${m}GeV_ctau_${ct}cm.txt ${folder}/datacard_metCutEq100_ptGt30_Le50_ECaloCutEq5_IasGt0p30_Le0p99_mass_${m}GeV_ctau_${ct}cm.txt ${folder}/datacard_metCutEq100_ptGt50_Le50000_ECaloCutEq5_IasGt0p05_Le0p30_mass_${m}GeV_ctau_${ct}cm.txt ${folder}/datacard_metCutEq100_ptGt50_Le50000_ECaloCutEq5_IasGt0p30_Le0p99_mass_${m}GeV_ctau_${ct}cm.txt   > datacardsCombined/datacard_mass_${m}GeV_ctau_${ct}cm.txt
 
     done
 done
 
-python makePrediction.py 30 50 0.05 0.30
-python makePrediction.py 30 50 0.30 0.99
-python makePrediction.py 50 50000 0.05 0.30
-python makePrediction.py 50 50000 0.30 0.99
+python makePrediction.py 30 50 0.05 0.30 ${folder}
+python makePrediction.py 30 50 0.30 0.99 ${folder}
+python makePrediction.py 50 50000 0.05 0.30 ${folder}
+python makePrediction.py 50 50000 0.30 0.99 ${folder}
 
