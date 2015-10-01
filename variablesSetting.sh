0a260b51ec1ea398b5045d3faf0a06f04a824ba4
Teresa Lenz <teresa.lenz@desy.de>
2015-10-01 14:46:24 +0200

    nicer style

--- variablesSetting.sh
+++ variablesSetting.sh
@@ -1,26 +1,20 @@
 #!/bin/bash
 
-metCut=100
-jetptCut=110
-ecaloCut=5
-ptCutMin=30
-ptCutMax=50000
-iasCutMin=0.05
-iasCutMax=0.99
-region="SR"
+#metCut=100
+#jetptCut=110
+#ecaloCut=5
+#ptCutMin=30
+#ptCutMax=50
+#iasCutMin=0.05
+#iasCutMax=0.30
+#region="SR"
 
 
-mass=("100" "200" "300" "400" "500")
-ctau=("1" "5" "10" "50" "100" "1000")                           #please specifiy in cm
+mass=("100" "200" "300" "400" "500" "600")
+ctau=("1" "2" "3" "4" "5" "6" "7" "8" "9" "10" "20" "30" "40" "50" "60" "70" "80" "90" "100" "200" "300" "400" "500" "600" "700" "800" "900" "1000" "2000" "3000" "4000" "5000" "6000" "7000" "8000" "9000" "10000")
 
 echo "config file is read"
 
-#echo "jetPt cut = " $jetptCut
-#echo "MET cut   = " $metCut
-#echo "Ecalo cut = " $ecaloCut
-#echo "Pt cut    = " $ptCutMin - $ptCutMax
-#echo "Ias cut   = " $iasCutMin - $iasCutMax
-#echo "region    = " $region
 
 
 
