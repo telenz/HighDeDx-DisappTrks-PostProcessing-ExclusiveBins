d59f11301a9aae73073e71869cb944ce542477fa
Teresa Lenz <teresa.lenz@desy.de>
2015-10-13 12:58:48 +0200

    updated filename

--- limits/CMSSW_5_3_20/src/HiggsAnalysis-CombinedLimit/runAllLimits.sh
+++ limits/CMSSW_5_3_20/src/HiggsAnalysis-CombinedLimit/runAllLimits.sh
@@ -7,7 +7,7 @@ decaywidthTable=("1" "2" "3" "4" "5" "6" "7" "8" "9" "10" "20" "30" "40" "50" "6
 
 
 
-filenameIn="datacardsCombined_10_11"
+filenameIn="datacardsCombined"
 mkdir logFiles/${filenameIn}
 mkdir limits/${filenameIn}
 
